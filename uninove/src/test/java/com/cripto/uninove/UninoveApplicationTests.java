de63fe3a36323084fffd892b22189e42bc2d7cb9
OliverRodrigues <100792569+OliverRodrigues@users.noreply.github.com>
2023-09-17 22:35:44 -0300

    Commit inicial projeto em Spring

---
+++ uninove/src/test/java/com/cripto/uninove/UninoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cripto.uninove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UninoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
